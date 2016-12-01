32e519ee036ce0297fc29200f7386f774ed3db6b
叶嘉雨 <yejiayu.fe@gmail.com>
2016-12-01 17:44:04 +0800

    resources: 删除example, 因为已经有例子工程了

--- example/logger/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const AMSLogger = require('@ali/ams-logger');
-
-const amsLogger = AMSLogger.init({
-  appName: 'cluster',
-});
-
-module.exports = category => amsLogger.getLogger(category);
