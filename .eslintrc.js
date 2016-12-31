b1ac3663d4f1fdd80d5968a8bd32a5343b8f8534
叶嘉雨 <yejiayu.fe@gmail.com>
2017-01-01 02:43:04 +0800

    resources: add babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
-  'parserOptions':false,
+  "parser": "babel-eslint",
+  'parserOptions': {
+    'sourceType': 'script',
+  },
+  "plugins": [
+    "babel"
+  ],
   'rules': {
     "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": false}],
     'strict': [2, 'global'],
+    'global-strict': [0, 'always'],
     'no-underscore-dangle': ["error", { "allowAfterThis": true }],
     'no-use-before-define': ["error", { "functions": false, "classes": true }],
-    'no-param-reassign': [0],
+    "generator-star-spacing": 0
   },
   'extends': 'airbnb-base',
   'env': {
     'node': true,
     'mocha': true,
+    "es6": true,
   },
 }
