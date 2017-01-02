d2bc177411e2700f9c88656a36aaad03779a3e11
叶嘉雨 <yejiayu.fe@gmail.com>
2017-01-02 20:57:46 +0800

    resources: 屏蔽app worker细节后的例子

--- example/index.js
+++ example/index.js
@@ -1,14 +1,3 @@
 'use strict';
 
-const debug = require('debug')('cluster-dispatch:app');
-const co = require('co');
-
-const AppClient = require('../').AppClient;
-
-co(function* gen() {
-  const appClient = new AppClient({ logging: debug });
-  yield appClient.init();
-  appClient.on('error', debug);
-
-  require('./web');
-}).catch(debug);
+require('./web');
